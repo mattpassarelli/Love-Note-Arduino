39e8dc46ff487fdd1530678ede7929adc51a6aaa
Matt Passarelli <mattvpassarelli@gmail.com>
2020-04-25 17:29:55 -0400

    Have working WiFi and maybe connections. Blocked myself out of the github API for a few hours, so I won't know for a while

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,176 @@
 // Love Box
 //
 #include <U8x8lib.h>
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
+#include <WiFiClientSecure.h>
+#include "secrets.h"
+
 U8X8_SH1106_128X64_NONAME_HW_I2C oled(/* reset=*/U8X8_PIN_NONE);
+bool isConnectedToWifi = false;
+bool wasMessageRead = false;
+char idSaved = -1; 
+String line;
+String modeSelector;
+const char* ssid = _ssid;
+const char* password = _password;
+const String url = _url;
+WiFiClientSecure client;
+
+// Defined in "CACert" tab.
+extern const unsigned char caCert[] PROGMEM;
+extern const unsigned int caCertLen;
+
+
+
+
+
+
+
+
+void wifiConnect() {
+  if (WiFi.status() != WL_CONNECTED) {
+    oled.drawString(0, 0, "Connecting to WiFi network...");
+    WiFi.begin(ssid, password);
+  
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
+  }
+}
+
+void drawMessage(const String& message) {
+  oled.clear();
+
+  if(modeSelector[0] == 't'){
+    oled.drawString(0, 0, message.c_str());    
+  } 
+  else {
+    for(int i = 0; i <= message.length(); i++){
+      int x = i % 129;
+      int y = i / 129;
+    
+      if(message[i] == '1'){
+        oled.draw1x2Glyph(x, y, 1);
+      }
+    } 
+  }    
+  oled.display();
+}
+
+void waitForMessage()
+{
+  Serial.println("in message function");
+  const int httpsPort = 443;
+  const char *host = "gist.githubusercontent.com";
+  const char fingerprint[] = "CC AA 48 48 66 46 0E 91 53 2C 9C 7C 23 2A B1 74 4D 29 9D 33";
+  Serial.print("Looking to host ");
+  Serial.println(host);
+
+  if (!client.connect(host, httpsPort))
+  {
+    Serial.println("Connection to host failed");
+  } else {
+    Serial.println("Server Certificated verified");
+  }
+
+  Serial.println("Requesting URL: " + url);
+
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "User-Agent: ESP8266\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    Serial.println("Request Sent");
+
+    while (client.connected())
+    {
+      String temp = client.readStringUntil('\n');
+      if (temp == "\r")
+      {
+        Serial.println("Headers Received");
+        break;
+      }
+    }
+    String id = client.readStringUntil('\n');
+
+    Serial.println("String received from client is: " + client.readString());
+
+    Serial.println("ID Received was: " + id[0]);
+    if (id[0] != idSaved)
+    {
+      wasMessageRead = false;
+      idSaved = id[0];
+      EEPROM.write(142, idSaved);
+      EEPROM.write(144, wasMessageRead);
+      EEPROM.commit();
+
+      modeSelector = client.readStringUntil('\n');
+      line = client.readStringUntil(0);
+      drawMessage(line);
+    }
+  
+}
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   oled.begin();
   oled.setFont(u8x8_font_victoriamedium8_r);
   oled.clear();
-  oled.drawString(0,0,"Hello World!");
+  oled.drawString(0,0,"Hello :)");
+
+  delay(1000);
+
+  wifiConnect();
+
+  oled.clear();
+  oled.drawString(0,0,"Setting up...");
+  oled.display();
+
+  // Synchronize time useing SNTP. This is necessary to verify that
+  // the TLS certificates offered by the server are currently valid.
+  Serial.print("Setting time using SNTP");
+  configTime(8 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+
+  // Load root certificate in DER format into WiFiClientSecure object
+  bool res = client.setCACert_P(caCert, caCertLen);
+  if (!res) {
+    Serial.println("Failed to load root CA certificate!");
+    while (true) {
+      yield();
+    }
+  }
+
+  EEPROM.begin(512);
+  idSaved = EEPROM.get(142, idSaved);
+  wasMessageRead = EEPROM.get(144, wasMessageRead);
 }
 
 void loop()
-{
+{  
+  if(!wasMessageRead){
+    Serial.println("Waiting for message");
+    oled.clear();
+    oled.drawString(0, 0, "Waiting for a message");
+    oled.display();
+
+    waitForMessage();
+  }
+  else {
+    //Light up leds
+    //Display message/
+    //Find way to hide message
+  }
 }

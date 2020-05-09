69387e346a71a42e593cec613fd66a0f306e1080
Matt Passarelli <mattvpassarelli@gmail.com>
2020-05-09 18:26:38 -0400

    Added LED funtionality. Pulses when you have a new message. Add button to reset looking for message

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,15 @@
 #include <EEPROM.h>
 #include <WiFiClientSecure.h>
 #include "secrets.h"
+#include "FastLED.h"
 
+#define NUM_LEDS 18
+
+CRGB leds[NUM_LEDS];
 SH1106Wire oled(0x3c, D2, D1);
-int SCREEN_WIDTH = 128;
+const int SCREEN_WIDTH = 128;
 bool isConnectedToWifi = false;
-bool wasMessageRead = false;
+bool wasMessageRead = true;
 char idSaved = -1;
 String line;
 String modeSelector;
@@ -18,6 +22,12 @@ const char *ssid = _ssid;
 const char *password = _password;
 const String url = _url;
 WiFiClientSecure client;
+
+int fadeAmount = 5;
+int brightness = 0;
+
+const int readMessagePin = D5;
+const int LEDPin = D7;
 
 // Defined in "CACert" tab.
 extern const unsigned char caCert[] PROGMEM;
@@ -27,7 +37,7 @@ void wifiConnect()
 {
   if (WiFi.status() != WL_CONNECTED)
   {
-    oled.drawStringMaxWidth(0, 0,  SCREEN_WIDTH, "Connecting to WiFi network...");
+    oled.drawStringMaxWidth(0, 0, SCREEN_WIDTH, "Connecting to WiFi network...");
     WiFi.begin(ssid, password);
 
     while (WiFi.status() != WL_CONNECTED)
@@ -41,25 +51,25 @@ void drawMessage(const String &message)
 {
   oled.clear();
 
-//  if (modeSelector[0] == 't')
-//  {
-    oled.drawStringMaxWidth(0, 0,  SCREEN_WIDTH, message.c_str());
-//  }
-//  else
-//  {
-//    for (int i = 0; i <= message.length(); i++)
-//    {
-//      int x = i % 129;
-//      int y = i / 129;
-//
-//      if (message[i] == '1')
-//      {
-//        oled.draw1x2Glyph(x, y, 1);
-//      }
-//    }
-//  }
+  //  if (modeSelector[0] == 't')
+  //  {
+  oled.drawStringMaxWidth(0, 0, SCREEN_WIDTH, message.c_str());
+  //  }
+  //  else
+  //  {
+  //    for (int i = 0; i <= message.length(); i++)
+  //    {
+  //      int x = i % 129;
+  //      int y = i / 129;
+  //
+  //      if (message[i] == '1')
+  //      {
+  //        oled.draw1x2Glyph(x, y, 1);
+  //      }
+  //    }
+  //  }
   oled.display();
-  wasMessageRead = true;
+  wasMessageRead = false;
 }
 
 void waitForMessage()
@@ -117,17 +127,17 @@ void setup()
   oled.setColor(WHITE);
   oled.setTextAlignment(TEXT_ALIGN_LEFT);
   oled.setFont(ArialMT_Plain_16);
-  oled.drawStringMaxWidth(0, 0,  SCREEN_WIDTH, "Hello :)");
+  oled.drawStringMaxWidth(0, 0, SCREEN_WIDTH, "Hello :)");
 
   delay(1000);
 
   wifiConnect();
 
   oled.clear();
-  oled.drawStringMaxWidth(0, 0,  SCREEN_WIDTH, "Setting up...");
+  oled.drawStringMaxWidth(0, 0, SCREEN_WIDTH, "Setting up...");
   oled.display();
 
-  // Synchronize time useing SNTP. This is necessary to verify that
+  // Synchronize time using SNTP. This is necessary to verify that
   // the TLS certificates offered by the server are currently valid.
   Serial.print("Setting time using SNTP");
   configTime(8 * 3600, 0, "pool.ntp.org", "time.nist.gov");
@@ -155,26 +165,73 @@ void setup()
     }
   }
 
-  EEPROM.begin(512);
-  idSaved = EEPROM.get(142, idSaved);
-  wasMessageRead = EEPROM.get(144, wasMessageRead);
+  //Register Pin for button to mark message as read
+  pinMode(readMessagePin, INPUT_PULLUP);
+
+  //LED Setup
+  FastLED.addLeds<NEOPIXEL, LEDPin>(leds, NUM_LEDS);
+  //Set LEDS to off
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Black;
+  }
+  FastLED.show();
+
+  // EEPROM.begin(512);
+  // idSaved = EEPROM.get(142, idSaved);
+  // wasMessageRead = EEPROM.get(144, wasMessageRead);
+}
+
+void fadeLEDS()
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i].setRGB(255, 0, 0); // setRGB functions works by setting
+                               // (RED value 0-255, GREEN value 0-255, BLUE value 0-255)
+                               // RED = setRGB(255,0,0)
+                               // GREEN = setRGB(0,255,0)
+    leds[i].fadeLightBy(brightness);
+  }
+  FastLED.show();
+  brightness = brightness + fadeAmount;
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness == 0 || brightness == 255)
+  {
+    fadeAmount = -fadeAmount;
+  }
+  delay(40); // This delay sets speed of the fade. I usually do from 5-75 but you can always go higher.
 }
 
 void loop()
 {
-  if (!wasMessageRead)
+  if (wasMessageRead)
   {
     Serial.println("Waiting for message");
     oled.clear();
-    oled.drawStringMaxWidth(0, 0,  SCREEN_WIDTH, "Waiting for a message");
+    oled.drawStringMaxWidth(0, 0, SCREEN_WIDTH, "Waiting for a message");
     oled.display();
 
     waitForMessage();
   }
   else
   {
-    //Light up leds
-    //Display message/
-    //Find way to hide message
+    //If the button is pressed, and the message is showing, then reset waiting for one
+    //Why it's reversed and I'm waiting for LOW, I Have no idea
+    if (digitalRead(readMessagePin) == LOW)
+    {
+      Serial.println("Setting wasMessageRead to true. Beginning to wait for a message again...");
+      //Set LEDS to off
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Black;
+      }
+      FastLED.show();
+      wasMessageRead = true;
+    }
+    else
+    {
+      //Light up LEDS until Message is read
+      fadeLEDS();
+    }
   }
 }
